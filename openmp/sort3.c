4c09e1d7797a62c64f99502b2cbb4655d27aecd9
Paweł Kopel <pawel.kopel2@gmail.com>
2022-04-09 16:25:19 +0200

    First working sort3

--- openmp/sort3.c
+++ openmp/sort3.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "bucket.h"
 
 #define N_BUCKETS 10
@@ -51,19 +52,30 @@ double split_buckets(uint* array, int size, bucket** buckets, int threads) {
     return end - start;
 }
 
-double sort_buckets(bucket** buckets, int threads) {
+double merge_buckets(bucket** buckets, int threads) {
     bucket b;
-    int i, my_n;
+    uint* s;
+    int i, sum = 0;
     double start, end;
 
     start = omp_get_wtime();
-#pragma omp parallel num_threads(threads) private(i, my_n, b) shared(buckets)
+#pragma omp parallel num_threads(threads) private(i, sum, b, s) shared(buckets)
     {
-        my_n = omp_get_thread_num();
 #pragma omp for schedule(static)
         for (i = 0; i < N_BUCKETS; i++) {
-            b = buckets[my_n][i];
-            insert_sort(b.array, b.size);
+            for (int j = 0; j < threads; j++) {
+                sum += buckets[j][i].size;
+            }
+            b = new_bucket(sum);
+            for (int j = 0; j < threads; j++) {
+                s = &b.array[b.size];
+                memcpy(s, buckets[j][i].array, buckets[j][i].size * sizeof(uint));
+                b.size += buckets[j][i].size;
+                // clean up unused buckets
+                free((void*)buckets[j][i].array);
+                buckets[j][i].size = 0;
+            }
+            buckets[0][i] = b;
         }
     }
     end = omp_get_wtime();
@@ -71,20 +83,49 @@ double sort_buckets(bucket** buckets, int threads) {
     return end - start;
 }
 
-double merge_buckets(uint* array, bucket** buckets, int threads) {
+double sort_buckets(bucket** buckets, int threads) {
     bucket b;
+    int i;
     double start, end;
 
     start = omp_get_wtime();
-    for (int i = 0; i < N_BUCKETS; i++) {
+#pragma omp parallel for schedule(static) num_threads(threads) private(i, b) shared(buckets)
+    for (i = 0; i < N_BUCKETS; i++) {
         b = buckets[0][i];
-        printf("bucket %d %d\n", i, b.size);
-        for (int j = 0; j < b.size; j++) {
-            printf("%d", b.array[j]);
+        insert_sort(b.array, b.size);
+    }
+    end = omp_get_wtime();
+
+    return end - start;
+}
+
+double merge_array(uint* array, bucket** buckets, int threads) {
+    uint* s;
+    int i, j, size;
+    double start, end;
+
+    start = omp_get_wtime();
+// for (int i = 0; i < N_BUCKETS; i++) {
+//     printf("%d ", size);
+//     s = &array[size];
+//     memcpy(s, buckets[0][i].array, buckets[0][i].size * sizeof(uint));
+//     size += buckets[0][i].size;
+// }
+#pragma omp parallel num_threads(threads) private(i, j, s, size) shared(array, buckets)
+    {
+#pragma omp for schedule(static)
+        for (i = 0; i < N_BUCKETS; i++) {
+            size = 0;
+            for (j = 0; j < i; j++) {
+                size += buckets[0][j].size;
+            }
+            printf("%d ", size);
+            s = &array[size];
+            memcpy(s, buckets[0][i].array, buckets[0][i].size * sizeof(uint));
         }
     }
-
     end = omp_get_wtime();
+    printf("\n");
 
     return end - start;
 }
@@ -110,16 +151,18 @@ int main(int argc, char** argv) {
 
     double time_fill = fill_array(array, size, threads);
     double time_split = split_buckets(array, size, buckets, threads);
+    double time_merge_b = merge_buckets(buckets, threads);
     double time_sort = sort_buckets(buckets, threads);
-    double time_merge = 0;  // merge_buckets(array, buckets, threads);
+    double time_merge_a = merge_array(array, buckets, threads);
 
     if (!debug) {
-        printf("%d,%f,%f,%f,%f\n",
+        printf("%d,%f,%f,%f,%f,%f\n",
                threads,
                time_fill,
                time_split,
+               time_merge_b,
                time_sort,
-               time_merge);
+               time_merge_a);
     } else {
         for (int i = 0; i < size; i++) {
             printf("%d\n", array[i]);
@@ -127,10 +170,10 @@ int main(int argc, char** argv) {
     }
 
     free((void*)array);
+    for (int i = 0; i < N_BUCKETS; i++) {
+        free((void*)buckets[0][i].array);
+    }
     for (int j = 0; j < threads; j++) {
-        for (int i = 0; i < N_BUCKETS; i++) {
-            free((void*)buckets[j][i].array);
-        }
         free((void*)buckets[j]);
     }
     free((void*)buckets);
