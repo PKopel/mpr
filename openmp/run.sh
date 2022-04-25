82fe68656642df7b2bdf6d861d0567acd373edab
Paweł Kopel <pawel.kopel2@gmail.com>
2022-04-26 00:28:46 +0200

    Finish sort3

--- openmp/run.sh
+++ openmp/run.sh
@@ -2,7 +2,6 @@
 
 echo "size,n_threads,n_buckets,fill_time,split_time,merge_b_time,sort_time,merge_a_time,time_total,sorted"> results_1.csv
 
-
 for i in 1; do
     for b in 10 20 30 40 50 100 150 200 250 300; do
         for s in 1000000 5000000; do
@@ -10,6 +9,17 @@ for i in 1; do
                 echo -n "$s," >> results_1.csv
                 make run-$1 ARGS="$i $b $s" >> results_1.csv
             done
+        done
+    done
+done
+
+echo "size,n_threads,n_buckets,fill_time,split_time,merge_b_time,sort_time,merge_a_time,time_total,sorted"> results_2.csv
+
+for i in {1..10}; do
+    for s in 1000000 5000000; do
+        for _ in {1..10}; do
+            echo -n "$s," >> results_2.csv
+            make run-$1 ARGS="$i 50 $s" >> results_2.csv
         done
     done
 done
