f3061676b6eb02395d06363631ba45fc9851190e
Paweł Kopel <pawel.kopel2@gmail.com>
2022-04-25 22:04:23 +0200

    Further improve sort3

--- openmp/run.sh
+++ openmp/run.sh
@@ -4,8 +4,8 @@ echo "size,n_threads,n_buckets,fill_time,split_time,merge_b_time,sort_time,merge
 
 
 for i in 1; do
-    for b in 300 350 400; do
-        for s in 100000 500000 1000000; do
+    for b in 10 20 30 40 50 100 150 200 250 300 350 400; do
+        for s in 1000000 5000000; do
             for _ in {1..10}; do
                 echo -n "$s," >> results_1.csv
                 make run-$1 ARGS="$i $b $s" >> results_1.csv
